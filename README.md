0e3c36c574acf361d84f77776c7639bb4a6dd831
mhx <mhx@163.com>
2018-04-27 10:06:32 +0800

    1

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
 # stools
+
+#### STools 组件相比Tools 包含依赖db的sql操作，这部分功能依赖db组件 含如下方法： ####
+
+1. static Obj( db )  选择数据库同配置文件 （ 默认default ） ,
+唯一的静态方法，通常作为前行使用。
+返回对象本身的实例化。
+2. table( table ) 设置表名 ， 当table等于数组时 设置一组表名
+返回值：同上。
+3. columns ( columns ) 设置 field ， 当columns 等于数组时 设置一组field ，同table对应。 注： 如果table是一组数据 而此值是一个数据，那么此值将统一与每个table所对应。
+返回值：同上。
+4. where( where ) 设置where 同上 ， 不同的是 where本身为数组 如 [ id => 1]  ， 对应多个表时可以设置成 [[id=>1],[id=>2]]。
+返回值：同上。
+5. Page( page , pagesize ) 通过上述设置 直接进行分页查询 示例：
+
+```
+
+        STools::Obj()->table(  'test1' )->where( ['id[<]' => 10 ])->Page($page , 4);
+         [
+                    'count' => 0,
+                    'data' => [],
+                    'page' => 1,
+                    'pagesize' => 10,
+                    'maxpage' => 0,
+          ];
+
+```
+
+6. multiPage () 参数与page相同 如下：
+
+```
+        STools::Obj()->table( [ 'test1' , 'test2' ])->where( ['id[<]' => 10 ])->multiPage($page , 4);
+
+        [
+            'unitC' => [], //各结果集总数 ， 索引数组 循序同 table
+            'count' => 0, //结果总数
+            'data' => [], 数据
+            'page' => 1,
+            'pagesize' => 10,
+            'maxpage' => 0,
+        ];
+
+        注： 由于返回结果是由多个结果集拼接上的 ，在多个表field 不一致时 ， 可以通过 columns 将返回值指定成相同名称。
+
+```
+
+
+#### MDb 组件内调用 ， 对 db组件进行的二次封装。 ####
+
+```
+插入唯一数据
+Base\MDb::e()->uniqueInsert('shechem_test1' , [
+            'note1' => 'unique2'
+            ] , '`note1`='.Base\MDb::e()->quote('unique2'));
+
+  成功返回 id 否则 返回 0
+  注： 插入的数据必须被 unique 含盖
+```
---
+++ src/Base/STools.php
@@ -0,0 +1,187 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mhx
+ * Date: 2017/9/29
+ * Time: 10:48
+ */
+
+namespace ATSoft\Tools\Base;
+
+
+/**
+ * Class STools
+ * @package ATSoft\Tools\Base
+ *
+ * 这个类依赖 db组件  和 基础类的方法
+ * 提供和数据库相关的服务
+ *
+ */
+class STools
+{
+
+    //静态变量保存全局实例
+    private static $_instance = null;
+    private $showError = true;
+
+    public $dbPre = [];
+    public $dbTable = [];
+    public $dbJoin = [];
+    public $dbColumns = [];
+    public $dbWhere = [];
+
+    //私有构造函数，防止外界实例化对象
+    private function __construct() {
+        $this->initV();
+    }
+
+    private function initV(){
+        $this->dbPre = ['_'];
+        $this->dbTable = [''];
+        $this->dbColumns = ['*'];
+        $this->dbWhere = [[]];
+        $this->dbJoin = [];
+    }
+
+    //私有克隆函数，防止外办克隆对象
+    private function __clone() {
+    }
+
+    //静态方法，单例统一访问入口
+    static private function getInstance() {
+        if ( !self::$_instance instanceof self) {
+            self::$_instance = new self ();
+        }
+        return self::$_instance;
+    }
+
+
+    /**
+     * mhx
+     * 说明：设置数据库
+     * @param null $dbPre
+     * @return STools|null
+     * @throws \Exception
+     */
+    static  function Obj($dbPre = null){
+
+        $obj = self::getInstance();
+        if(is_array($dbPre)){
+            if(!Tools::is_indexArray( $dbPre )) throw new \Exception( 'STools where throw : $dbPre must is indexed array!');
+            foreach( $obj->dbPre as $k=>$v ){
+                $obj->dbPre[$k] = $v;
+            }
+        }else{
+            $obj->dbPre[0] = $dbPre;
+        }
+
+        return $obj;
+    }
+
+    function table( $table ){
+
+        if(is_array( $table )){
+            if(!Tools::is_indexArray( $table )) throw new \Exception( 'STools where throw : $table must is indexed array!');
+//            foreach( $table as $k=>$v ){
+//                $this->dbTable[$k] = $v;
+//            }
+            $this->dbTable = $table;
+        }else{
+            $this->dbTable[0] = $table;
+        }
+
+        return $this;
+    }
+
+    function join( $join ){
+        if( Tools::is_indexArray( $join ) ){
+            $this->dbJoin = $join;
+        }else{
+            $this->dbJoin[0] = $join;
+        }
+        return $this;
+    }
+
+    function columns( $columns = '*' ){
+
+        if( Tools::is_indexArray( $columns ) && Tools::is_indexArray( $columns[0] ) ){
+            $this->dbColumns = $columns;
+        }else{
+            $this->dbColumns[0] = $columns;
+        }
+
+        return $this;
+    }
+
+    function where( $where = null ){
+        if( Tools::is_indexArray( $where ) ){
+            foreach( $where as $k=>$v ){
+                $this->dbWhere[$k] = $v;
+            }
+        }else{
+            $this->dbWhere[0] = $where;
+        }
+        return $this;
+    }
+
+    public function Page( $page = 1 , $pagesize= 10 , $debug = false){
+
+        $count = MDb::e( $this->dbPre[0] )->count($this->dbTable[0] , $this->dbWhere[0]);
+
+        if($debug){
+            echo MDb::e( $this->dbPre[0] )->last_query() . PHP_EOL;
+        }
+
+        $page = Tools::page($count , $page ,$pagesize);
+        $this->dbWhere[0]['LIMIT'] = $page['limit'];
+
+        if( empty( $this->dbJoin ) ){
+            $data = MDb::e( $this->dbPre[0] )->select( $this->dbTable[0] , $this->dbWhere[0] ,$this->dbColumns[0] );
+        }else{
+            $data = MDb::e( $this->dbPre[0] )->select( $this->dbTable[0] , $this->dbJoin[0] ,$this->dbColumns[0] ,$this->dbWhere[0] );
+        }
+
+
+        if($debug){
+            echo MDb::e( $this->dbPre[0] )->last_query() . PHP_EOL;
+        }
+
+        return [
+            'count' => $count,
+            'list' => $data,
+            'page' => $page['page'],
+            'pagesize' => $page['pagesize'],
+            'maxpage' => $page['maxpage'],
+        ];
+    }
+
+    public function multiPage( $page = 1 , $pagesize= 10 ){
+        $count = [];
+        foreach( $this->dbTable as $k => $v ){
+            $count[$k] = MDb::e( $this->_IfNullFirstVal('dbPre' , $k) )->count($this->dbTable[$k] , $this->_IfNullFirstVal('dbWhere' , $k));
+        }
+        $multiPage = Tools::multiPage($count , $page ,$pagesize);
+        $rData = [];
+        foreach( $this->dbTable as $k => $v ){
+            if($multiPage['limit'][$k]){
+                $where = $this->_IfNullFirstVal('dbWhere' , $k);
+                $where['LIMIT'] = $multiPage['limit'][$k];
+                $data =  MDb::e( $this->_IfNullFirstVal('dbPre' , $k) )->select( $this->dbTable[$k] , $where  , $this->_IfNullFirstVal('dbColumns' , $k) );
+                $rData = array_merge( $rData , $data);
+            }
+        }
+        return [
+            'unitC' => $count,
+            'count' => $multiPage['count'],
+            'list' => $rData,
+            'page' => $multiPage['page'],
+            'pagesize' => $multiPage['pagesize'],
+            'maxpage' => $multiPage['maxpage'],
+        ];
+    }
+
+    private function _IfNullFirstVal( $property , $key ){
+        return empty($this->$property[$key]) ? $this->$property[0] : $this->$property[$key];
+    }
+
+}
